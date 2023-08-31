ab4d93fa6f6e8fbe92198550fd01e0cb3049cdf6
tanay rai <96621818+nightyol@users.noreply.github.com>
2023-09-01 00:46:42 +0530

    Add files via upload

---
+++ scripts/mint.js
@@ -0,0 +1,31 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+const tokenContractJSON = require("../artifacts/contracts/StakeEth.sol/StakeEth.json");
+require('dotenv').config()
+
+const tokenAddress = "0x57a03F44f77ffDB992429432a2c45a5ab804EF9d"; // place your erc20 contract address here
+const tokenABI = tokenContractJSON.abi;
+const walletAddress = "0x19303b6bda13b97198c2836442fC85c3946DD57F"; // place your public address for your wallet here
+
+async function main() {
+
+    const token = await hre.ethers.getContractAt(tokenABI, tokenAddress);
+    for(let i=0; i<5; i++)
+    {
+    const tx = await token.mint(walletAddress);
+    await tx.wait();
+    }
+  console.log("You now have: " + await token.balanceOf(walletAddress) + " tokens");
+  }
+  
+  // We recommend this pattern to be able to use async/await everywhere
+  // and properly handle errors.
+  main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
